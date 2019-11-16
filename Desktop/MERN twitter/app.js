b459761c3c32c9a63bb63127c476994d2cf8c003
msong793y <miles.song.ca@gmail.com>
2019-11-16 15:38:55 -0800

    set up basic express

--- Desktop/MERN twitter/app.js
+++ Desktop/MERN twitter/app.js
@@ -2,6 +2,12 @@ const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const db = require("./config/keys").mongoURI;
+const bodyParser = require("body-parser");
+
+
+//routes
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -9,8 +15,18 @@ mongoose
   .catch(err => console.log(err));
 
 
-app.get("/", (req, res) => res.send("whats up yo"));
+app.get("/", (req, res) => {
+        console.log(res);     
+        res.send("whats up yo")});
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //port info
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
+
