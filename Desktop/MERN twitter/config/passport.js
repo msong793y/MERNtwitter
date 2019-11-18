7a3a42a82359807cf93ed2e96215a16fb6c17abb
msong793y <miles.song.ca@gmail.com>
2019-11-18 09:57:06 -0800

    added in front end

--- Desktop/MERN twitter/config/passport.js
+++ Desktop/MERN twitter/config/passport.js
@@ -9,12 +9,6 @@ const options={};
 options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 options.secretOrKey = keys.secretOrKey;
 
-// module.exports = passport =>{ 
-//     passport.use(new JwtStrategy(options,(jwt_payload, done)=>{
-//         cons(jwt_payload);
-//         done();
-//     }))
-// }
 
 
 module.exports = passport => {
